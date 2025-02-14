296575fede89745e52e9fa14ade79d28871760a4
Anna Chizhik <53980712+Frantsuzova@users.noreply.github.com>
2025-02-14 15:10:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -33,9 +33,10 @@ $(document).ready(function() {
 
 
 
+
 document.addEventListener("DOMContentLoaded", function () {
   const width = 700;
-  const height = 500;
+  const height = 600;
 
  
 
@@ -268,5 +269,46 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+
+// Функция для создания облака слов
+function generateWordCloud() {
+    var words = [
+      {text: "Computational Linguistics", size: 20},
+      {text: "Corpus Linguistics", size: 15},
+      // Добавьте остальные слова и их размеры
+    ];
+  
+    var layout = d3.layout.cloud()
+      .size([300, 300])
+      .words(words.map(function(d) { return {text: d.text, size: d.size}; }))
+      .padding(5)
+      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact")
+      .fontSize(function(d) { return d.size; })
+      .on("end", draw);
+  
+    layout.start();
+  
+    function draw(words) {
+      d3.select("#word-cloud").append("svg")
+          .attr("width", layout.size()[0])
+          .attr("height", layout.size()[1])
+        .append("g")
+          .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+        .selectAll("text")
+          .data(words)
+        .enter().append("text")
+          .style("font-size", function(d) { return d.size + "px"; })
+          .style("font-family", "Impact")
+          .attr("text-anchor", "middle")
+          .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+          })
+          .text(function(d) { return d.text; });
+    }
+  }
+  
+ 
+  
 
 
